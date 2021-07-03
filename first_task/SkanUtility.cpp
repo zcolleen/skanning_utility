2e5395b5a5ed47a7974203334e0eee6e79f5a524
zcolleen <semeneigin@yandex.ru>
2021-07-03 17:54:40 +0300

    mutex added

--- first_task/SkanUtility.cpp
+++ first_task/SkanUtility.cpp
@@ -7,7 +7,15 @@ SkanUtility::SkanUtility(const char *directory) : _directory(directory), _errors
 _js_suspicious(0), _mac_suspicious(0), _unix_suspicious(0) {}
 
 
-void SkanUtility::_scan_file(std::string file_name)
+void SkanUtility::_write_data(std::mutex &mutex, size_t &data)
+{
+    mutex.lock();
+    ++data;
+    mutex.unlock();
+}
+
+void SkanUtility::_scan_file(std::string file_name, std::mutex &unix_mutex, std::mutex &mac_mutex, std::mutex &js_mutex,
+                             std::mutex &error_mutex)
 {
 	std::ifstream file(file_name);
 	std::string str;
@@ -17,22 +25,22 @@ void SkanUtility::_scan_file(std::string file_name)
 		while (getline(file, str)) {
 
 			if (str == UNIX_SUSPICIOUS) {
-				++_unix_suspicious;
+                _write_data(unix_mutex, this->_unix_suspicious);
 				break;
 			}
 			else if (str == MAC_SUSPICIOUS) {
-				++_mac_suspicious;
+                _write_data(mac_mutex, this->_mac_suspicious);
 				break;
 			}
 			else if (str == JS_SUSPICIOUS && file_name.substr(file_name.rfind('.') + 1) == JS_EXTENSION) {
-				++_js_suspicious;
+                _write_data(js_mutex, this->_js_suspicious);
 				break;
 			}
 		}
 		file.close();
 	}
 	else
-		++_errors;
+        _write_data(error_mutex, this->_errors);
 }
 
 void SkanUtility::_print_report(size_t number_of_files)
@@ -47,16 +55,17 @@ void SkanUtility::_print_report(size_t number_of_files)
 	"=========================" << std::endl;
 }
 
-void SkanUtility::f() {
-    std::cout << "KK" << std::endl;
-}
-
 void SkanUtility::sсan_directory()
 {
 	DIR *dir_stream;
 	struct dirent *entry;
 	size_t number_of_files = 0;
 	std::list<std::thread> threads;
+	std::mutex unix_mutex;
+	std::mutex mac_mutex;
+	std::mutex js_mutex;
+	std::mutex error_mutex;
+
 
 	if (!(dir_stream = opendir(_directory))) {
 		std::cout << "Can't open directory" << std::endl;
@@ -64,12 +73,12 @@ void SkanUtility::sсan_directory()
 	}
 	while ((entry = readdir(dir_stream))) {
 
-	    threads.push_back(std::thread(&SkanUtility::_scan_file, this, entry->d_name));
+	    threads.push_back(std::thread(&SkanUtility::_scan_file, this, entry->d_name, std::ref(unix_mutex),
+                                      std::ref(mac_mutex), std::ref(js_mutex), std::ref(error_mutex)));
 		++number_of_files;
 	}
-	for (auto it = threads.begin(); it != threads.end(); ++it) {
+	for (auto it = threads.begin(); it != threads.end(); ++it)
         (*it).join();
-	}
 	closedir(dir_stream);
 	_print_report(number_of_files);
 }
--- first_task/SkanUtility.hpp
+++ first_task/SkanUtility.hpp
@@ -7,6 +7,7 @@
 #include <dirent.h>
 #include <fstream>
 #include <thread>
+#include <mutex>
 #include <list>
 
 #define UNIX_SUSPICIOUS "rm -rf ~/Documents"
@@ -25,8 +26,10 @@ private:
     size_t _mac_suspicious;
     size_t _unix_suspicious;
 	void f();
-    void    _scan_file(std::string file_name);
+    void    _scan_file(std::string file_name, std::mutex &unix_mutex, std::mutex &mac_mutex, std::mutex &js_mutex,
+                       std::mutex &error_mutex);
 	void	_print_report(size_t number_of_files);
+    void    _write_data(std::mutex &mutex, size_t &data);
 
 public:
 	SkanUtility(const char *directory);
