313679d38ca1af72cd84cb63b900b758fa632ffd
zcolleen <semeneigin@yandex.ru>
2021-07-05 14:38:18 +0300

    pull request

---
+++ cmake-build-debug/CMakeFiles/kaspersky_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaspersky_test.dir/first_task/ScanUtility.cpp.o"
+  "CMakeFiles/kaspersky_test.dir/first_task/main.cpp.o"
+  "kaspersky_test"
+  "kaspersky_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaspersky_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
